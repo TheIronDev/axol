5740f43b2e93a4b6dbfe6d589a615c20f7abef22
Tyler Stark <theirondeveloper@gmail.com>
2017-12-05 21:22:44 -0800

    test(CanvasRender) CanvasItemRenderer
    This cl adds tests for the `canvasItemRenderer` method of the
    `canvas-render` component. This is going to be part of a multi-cl
    process to get better coverage of the different functions in
    CanvasItemRenderer.

--- src/canvas-render-test.js
+++ src/canvas-render-test.js
@@ -13,8 +13,28 @@ describe('canvas-render', () => {
     };
     mockCtx = {
       canvas: mockCanvas,
+      arc: sinon.spy(),
+      beginPath: sinon.spy(),
       clearRect: sinon.spy(),
+      closePath: sinon.spy(),
+      fill: sinon.spy(),
+      fillRect: sinon.spy(),
+      lineTo: sinon.spy(),
+      moveTo: sinon.spy(),
+      rotate: sinon.spy(),
+      stroke: sinon.spy(),
+      translate: sinon.spy(),
     };
+  });
+
+  describe('clearCanvas', () => {
+    it('should clear canvas', () => {
+      canvasRender.clearCanvas(mockCtx);
+
+      const isClearCalledWith = mockCtx.clearRect
+          .calledWith(0, 0, mockCanvas.width, mockCanvas.height);
+      expect(isClearCalledWith).to.be(true);
+    });
   });
 
   describe('drawCanvas', () => {
@@ -24,17 +44,36 @@ describe('canvas-render', () => {
       canvasRender.drawCanvas(mockCtx, canvasItemList);
       const isClearCalledWith = mockCtx.clearRect
           .calledWith(0, 0, mockCanvas.width, mockCanvas.height);
-      expect(isClearCalledWith).to.eql(true);
+      expect(isClearCalledWith).to.be(true);
     });
   });
 
-  describe('clearCanvas', () => {
-    it('should clear canvas', () => {
-      canvasRender.clearCanvas(mockCtx);
+  describe('canvasItemRender', () => {
+    it('should return a function that handles rendering canvasItems', () => {
+      const renderer = canvasRender.canvasItemRenderer(mockCtx);
+      const mockCanvasItem = {
+        endY: 50,
+        fillColor: '#f0f',
+        lineColor: '#0f0',
+        lineWidth: 2,
+        rotate: 0,
+        startX: 50,
+      };
+      renderer(mockCanvasItem);
 
-      const isClearCalledWith = mockCtx.clearRect
-          .calledWith(0, 0, mockCanvas.width, mockCanvas.height);
-      expect(isClearCalledWith).to.eql(true);
+      expect(mockCtx.fillStyle).to.be(mockCanvasItem.fillColor);
+      expect(mockCtx.strokeStyle).to.be(mockCanvasItem.lineColor);
+      expect(mockCtx.lineWidth).to.be(mockCanvasItem.lineWidth);
+    });
+
+    it('should do nothing if passed null or undefined', () => {
+      const renderer = canvasRender.canvasItemRenderer(mockCtx);
+
+      renderer(null);
+      expect(mockCtx.fillStyle).to.be(undefined);
+
+      renderer();
+      expect(mockCtx.fillStyle).to.be(undefined);
     });
   });
 });
